b31a5c88d8d680d9807f519b1b201bdbaa87705c
Steve Mussmann <17727337+stevemussmann@users.noreply.github.com>
2024-08-21 09:04:39 -0700

    Add files via upload

---
+++ seqDepth.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]
+then
+
+	echo "Usage: seqDepth.sh <infile> <outfile>"
+	exit
+
+fi
+
+FILE=$1
+OUT=$2
+
+paste \
+<(bcftools query -f '[%SAMPLE\t]\n' $FILE | head -1 | tr '\t' '\n') \
+<(bcftools query -f '[%DP\t]\n' $FILE | awk -v OFS="\t" '{for (i=1;i<=NF;i++) if ($i != ".") {sum[i]+=1; dp[i]+=$i} } END {for (i in sum) print i, dp[i] / sum[i] }' | sort -k1,1n | cut -f 2) > $OUT
+#<(bcftools query -f '[%DP\t]\n' $FILE | awk -v OFS="\t" '{for (i=1;i<=NF;i++) if ($i != ".") sum[i]+=1 } END {for (i in sum) print i, sum[i] / NR }' | sort -k1,1n | cut -f 2) > $OUT
+
+exit
