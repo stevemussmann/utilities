792a1b2374f126e91348abd649be4af1bd54c5c7
Steve Mussmann <17727337+stevemussmann@users.noreply.github.com>
2026-01-12 13:49:47 -0800

    seqDepth.sh now handles individuals with no data

--- seqDepth.sh
+++ seqDepth.sh
@@ -13,6 +13,6 @@ OUT=$2
 
 paste \
 <(bcftools query -f '[%SAMPLE\t]\n' $FILE | head -1 | tr '\t' '\n') \
-<(bcftools query -f '[%DP\t]\n' $FILE | awk -v OFS="\t" '{for (i=1;i<=NF;i++) if ($i != ".") {sum[i]+=1; dp[i]+=$i} } END {for (i in sum) print i, dp[i] / sum[i] }' | sort -k1,1n | cut -f 2) > $OUT
+<(bcftools query -f '[%DP\t]\n' $FILE | awk -v OFS="\t" '{for (i=1;i<=NF;i++) if ($i != ".") {sum[i]+=1; dp[i]+=$i} else {sum[i]+=0; dp[i]+=0} } END {for (i in sum) if (dp[i] == "0") {print i, 0.0} else { print i, dp[i] / sum[i] } }' | sort -k1,1n | cut -f 2) > $OUT
 
 exit
